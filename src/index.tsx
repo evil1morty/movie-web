739149d5f72b24f7b51f47af150fd13aa50c6c9d
Morty <42971559+evil1morty@users.noreply.github.com>
2023-11-19 16:06:14 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ import App from "@/setup/App";
 import { assertConfig, conf } from "@/setup/config";
 import i18n from "@/setup/i18n";
 
-import "@/setup/ga";
-import "@/setup/sentry";
+//import "@/setup/ga";
+//import "@/setup/sentry";
 import "@/setup/index.css";
 import "@/backend";
 import { initializeChromecast } from "./setup/chromecast";
